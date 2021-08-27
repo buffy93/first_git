27efb6428b1653c245270422137ff8a7cb954aa8
buffy <bouakran@web.de>
2021-08-27 11:45:49 +0200

    New main.py added to the git_project

---
+++ main.py
@@ -0,0 +1,26 @@
+#random code
+
+from geopy.geocoders import Nominatim
+from geopy import distance
+
+def distanceCalc(von, nach):
+    # API initialize
+    geoloc = Nominatim(user_agent="DistanceCalcRasa")
+
+
+    #Creating coordinates and the distance
+    von = geoloc.geocode(von)
+    nach = geoloc.geocode(nach)
+    von_lat, von_lon = (von.latitude), (von.longitude)
+    nach_lat, nach_lon = (nach.latitude), (nach.longitude)
+    von_location = (von_lat, von_lon)
+    nach_location = (nach_lat, nach_lon)
+    distances = distance.distance(von_location, nach_location).km
+
+    print(von)
+    print(nach)
+    print("-------------------------------------------------------------------------------")
+    print(str(round(distances, 2)))
+
+
+distanceCalc("bonn", "frankfurt")
