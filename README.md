11495e40816128d8ddf6cf473e71621ba51f1bb3
buffy <bouakran@web.de>
2021-09-02 10:59:44 +0200

    new branch with new function

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-#first time using git and github to learn how to work
+# first time using git and github to learn how to work
 
 # got some random code 
 
 # main = distance between 2 places
+
+#Complete city names of 2 given cities
--- main.py
+++ main.py
@@ -18,12 +18,24 @@ def distanceCalc(von, nach):
     nach_location = (nach_lat, nach_lon)
     distances = distance.distance(von_location, nach_location).km
 
-    print(von)
-    print(nach)
     print("-------------------------------------------------------------------------------")
     print("-------------------------------------------------------------------------------")
     print("-------------------------------------------------------------------------------")
     print(str(round(distances, 2)))
+    print("-------------------------------------------------------------------------------")
+    print("-------------------------------------------------------------------------------")
+    print("-------------------------------------------------------------------------------")
+
+def Cityname (von, nach):
+    # API initialize
+    geoloc = Nominatim(user_agent="DistanceCalcRasa")
+
+    von = geoloc.geocode(von)
+    nach = geoloc.geocode(nach)
+
+    print("First City : " + von)
+    print("-------------------------------------------------------------------------------")
+    print("Secound City : " + nach)
 
 
 distanceCalc("bonn", "frankfurt")
